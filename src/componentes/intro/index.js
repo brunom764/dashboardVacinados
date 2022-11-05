1f0a2698337bf6206aa76a507d14f9f9f6e0231d
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2022-11-05 12:30:42 -0300

    aba sobre

---
+++ src/componentes/intro/index.js
@@ -0,0 +1,13 @@
+import './intro.css'
+function Intro(){
+    return(
+        <section className="intro">
+            <h1 className="titulo">Sobre</h1>
+            <p className="texto textoNegrito">Projeto de visualização e ánalise de dados sobre a cobertura vacinal contra a covid-19 na cidade de Recife(PE) para a disciplina de Programação I 2022.1 UFPE. Neste projeto foi utilizado a linguagem python e as seguintes bibliotecas: pandas (preparo de dados), matplotlib (criação de gráficos e visualizações de dados em geral) e seaborn (plotagem de graficos). Além do mais, para a construção do dashboard, foi utilizado a biblioteca React js.</p>
+            <img className='zegotinha' alt='Ze gotinha' src='https://i.imgur.com/lpar1fK.png'/> 
+            <h1 className='titulo textoDestaque'>Vacine-se contra a Covid-19!</h1>
+        </section>
+    )
+}
+
+export default Intro
---
+++ src/pages/sobre/index.js
@@ -0,0 +1,13 @@
+import Header from "../../componentes/header"
+import Intro from "../../componentes/intro"
+
+function Sobre(){
+    return(
+        <>
+            <Header/>
+            <Intro/>
+        </>
+    )
+}
+
+export default Sobre
