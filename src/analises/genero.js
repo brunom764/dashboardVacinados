31deba33233eed3f1f242f47fa42d174fb82ff06
AnaJulia22 <anajulia11.o.fernandes@gmail.com>
2022-11-05 17:41:42 -0300

    nova pasta analises

---
+++ src/analises/genero.js
@@ -0,0 +1,25 @@
+const generos = [{
+    id:'0',
+    analise: 'Pegando como análise todas as vacinações realizadas, incluindo 1ª, 2ª, 3ª e 4ª doses, percebe-se ligeira maioria de mulheres entre as declarações de sexo. Resultado indica maior número de mulheres na população recifense.',
+    grafico: 'https://i.imgur.com/Ge8qKcj.png'
+},
+{
+    id:'1',
+    analise: 'Na 1ª dose a proporção indica uma ligeira semelhança se comparada à proporção de vacinação total, compreensível já que conforme visto na análise de números por doses, a 1ª dose representa uma adesão maior da população. Porém, uma diferença de aproximadamente 2% proporcionais de declarados homens e declaradas mulheres. A proporção de declarados Outros sexos continua ligeiramente a mesma, com um aumento de 0,1%.',
+    grafico: 'https://i.imgur.com/C97Dqxs.png'
+},
+{
+    id:'2',
+    analise: 'Na 2ª dose, a proporção volta a ser completamente igual comparado ao total, mas uma variação de aproximadamente 1,5% proporcional da adesão dos homens e mulheres da 1ª dose. A proporção de declarados Outros sexos continua a mesma.',
+    grafico: 'https://i.imgur.com/nH6Hiqg.png'
+},
+{
+    id:'3',
+    analise: 'Na 3ª dose, ocorre uma variação maior, a proporção de mulheres se torna uma significante maioria, levando entender que uma parcela de homens recifenses deixaram de se vacinar na 3ª dose. A proporção de declarados Outos sexos continua a mesma.',
+    grafico: 'https://i.imgur.com/RTzZ9mZ.png'
+},
+{
+    id:'4',
+    analise: 'Na 4ª dose, já se observa um cenário bem diferente, a maioria se vira para os declarados homens. Isso pode ser justificado devido a baixissíma quantidade de doses tomadas. Tornando inconclusivo a real proporção entre os sexos durante a 4ª dose.',
+    grafico: 'https://i.imgur.com/xLPZ8XY.png'
+}]
---
+++ src/analises/grupos.js
@@ -0,0 +1,30 @@
+const grupos = [{
+    id:'0',
+    analise: 'A partir do gráfico em barras percebe-se a grande variação no número de vacinas aplicadas entre as doses.',
+    grafico: 'https://i.imgur.com/5aC42L2.png'
+},
+{
+    id:'1',
+    analise: 'Na primeira dose, o grupo dos idosos foi o que mais tomou vacina dentre os grupos prioritários, vindo em segundo lugar o dos trabalhadores da saúde e terceiro pessoas com comorbidades.',
+    grafico: 'https://i.imgur.com/OzY3SMD.png'
+},
+{
+    id:'2',
+    analise: 'Na segunda dose da vacina os idosos continuam sendo a maioria dentre os prioritários.',
+    grafico: 'https://i.imgur.com/kWmrVmr.png'
+},
+{
+    id:'3',
+    analise: 'Na terceira dose houve uma redução significativa do número de vacinas aplicadas nos outros grupos prioritários. Dessa vez os idosos representam o grupo prioritário em mais da metade no número de vacinas tomadas.',
+    grafico: 'https://i.imgur.com/zMCHle4.png'
+},
+{
+    id:'4',
+    analise: 'Como na quarta dose houve uma redução significativa do número de vacinas aplicadas, então se foi divido em apenas dois, o de pessoas com comorbidades, no qual foi o grupo de prioritários que mais tomou vacina, e o restante em "outros".',
+    grafico: 'https://i.imgur.com/xEmq8ui.png'
+},
+{
+    id:'5',
+    analise: 'Analisando o gráfico, entende-se que o grupo que mais tomou vacina foi o comum.',
+    grafico: 'https://i.imgur.com/D4v9fwX.png'
+}]
