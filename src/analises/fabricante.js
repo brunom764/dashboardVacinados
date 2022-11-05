f696d631728476b6f702bebedec79520ba03f520
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2022-11-05 18:22:41 -0300

    Aba galeria

---
+++ src/analises/fabricante.js
@@ -0,0 +1,42 @@
+const fabricante = [{
+    id:'0',
+    analise: 'Observa-se um predomínio do uso da pfizer na cidade do Recife, uma vacina à base de mRNA para combater o vírus SARS-CoV-2 com eficácia global de 95%.',
+    grafico: 'https://i.imgur.com/6C0Ko3T.png'
+},
+{
+    id:'1',
+    analise: 'Na primeira dose, observa-se uma distribuição bem equilibrada dos tipos de vacinas aplicadas, com exceção da Janssen, que começou sua aplicação meses após as outras no Recife, e com a AstraZeneca tendo a maioria de doses aplicadas.',
+    grafico: 'https://i.imgur.com/02jBIFI.png'
+},
+{
+    id:'2',
+    analise: 'Na segunda dose, observa-se, também, uma distribuição bem equilibrada de aplicações entre AstraZeneca, CoronaVac e Pfizer, com a AstraZeneca, novamente, tendo a maioria de doses aplicadas e com a Janssen sendo a menos aplicada.',
+    grafico: 'https://i.imgur.com/TUz5ZfG.png'
+},
+{
+    id:'3',
+    analise: 'Na terceira dose, observa-se a aplicação predominantemente da Pfizer na população recifense. Uma observação que deve ser feita é que a vacina CORONAVAC - SINOVAC (BUTANTAN) não foi muito utilizada na terceira dose, sendo aplicada apenas em cerca de 200 recifenses. Isso é consequência de uma recomendação da Secretaria de Saúde do Recife, a qual recomendou a utilização da Pfizer, da Janssen e da AstraZeneca nas doses de reforço.',
+    grafico: 'https://i.imgur.com/XX4ifn4.png'
+},
+{
+    id:'4',
+    analise: 'Na quarta dose, observamos um cenário parecido com o da terceira dose com a vacina da Pfizer sendo a mais aplicada.',
+    grafico: 'https://i.imgur.com/KsWiF16.png'
+}]
+
+function Fabricantes(){
+    return(
+    <section>
+        <h1 className='titulo'>Analise por fabricantes</h1>
+        {fabricante.map(fabricante => {
+                const id = fabricante.id
+                return (
+                <div>
+                <img src={fabricante.grafico} alt={fabricante.titulo} className="a"/>
+                <p>{fabricante.analise}</p>
+                </div>
+                )})}
+    </section>
+    )
+}
+export default Fabricantes
---
+++ src/analises/geral.js
@@ -0,0 +1,22 @@
+const geral = [{
+    id:'0',
+    analise: 'Observa-se um dado preocupante na cobertura vacinal da população, porque há uma queda no número de doses aplicadas para a segunda dose e essa queda fica, ainda mais evidente, em comparação com a de doses aplicadas para terceira dose, pois das mais de 1.4 milhões de pessoas que tomaram a primeira dose, menos da metade tomou a terceira dose. Além disso, o gráfico apresenta a quarta dose, que tem sua aplicação em início com cerca de 1000 doses aplicadas.',
+    grafico: 'https://i.imgur.com/u51B4ii.png'
+}]
+
+function GeralAnalise(){
+    return(
+    <section>
+        <h1 className='titulo'>Analise</h1>
+        {geral.map(geral => {
+                const id = geral.id
+                return (
+                <div>
+                <img src={geral.grafico} alt={geral.titulo} className="a"/>
+                <p>{geral.analise}</p>
+                </div>
+                )})}
+    </section>
+    )
+}
+export default GeralAnalise
