aacd6d18ad3c780b32b03bbf6e165f2e308e1b61
Bruno Miguel Moreira Albuquerque <100159869+brunom764@users.noreply.github.com>
2022-11-06 17:59:58 -0300

    Adição de grafico

--- src/analises/geral.js
+++ src/analises/geral.js
@@ -1,8 +1,11 @@
-
 const geral = [{
     id:'0',
     analise: 'Observa-se um dado preocupante na cobertura vacinal da população, porque há uma queda no número de doses aplicadas para a segunda dose e essa queda fica, ainda mais evidente, em comparação com a de doses aplicadas para terceira dose, pois das mais de 1.4 milhões de pessoas que tomaram a primeira dose, menos da metade tomou a terceira dose. Além disso, o gráfico apresenta a quarta dose, que tem sua aplicação em início com cerca de 1000 doses aplicadas.',
     grafico: 'https://i.imgur.com/u51B4ii.png'
+},{
+    id:'1',
+    analise: 'Conclui-se que o número de doses da vacina contra a Covid-19 aplicadas durante a pandemia, no município de Recife-PE, se mostrou decadente. Isso se deu, possivelmente, por causa da retração da doença após as aplicações das primeiras e segundas doses, que fez com que os cidadãos julgassem não ser necessária a aplicação de uma terceira ou quarta dose.',
+    grafico: 'https://i.imgur.com/UPl0Ll1.png'
 }]
 
 function GeralAnalise(){
@@ -20,4 +23,3 @@ function GeralAnalise(){
     )
 }
 export default GeralAnalise
-
