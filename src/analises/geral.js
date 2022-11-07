64aab6cc728431c55f55250a08a8c4a1ff2fa6df
Bruno Miguel Moreira Albuquerque <100159869+brunom764@users.noreply.github.com>
2022-11-07 12:13:37 -0300

    Update geral.js

--- src/analises/geral.js
+++ src/analises/geral.js
@@ -1,6 +1,6 @@
 const geral = [{
     id:'0',
-    analise: 'Observa-se um dado preocupante na cobertura vacinal da população, porque há uma queda no número de doses aplicadas para a segunda dose e essa queda fica, ainda mais evidente, em comparação com a de doses aplicadas para terceira dose, pois das mais de 1.4 milhões de pessoas que tomaram a primeira dose, menos da metade tomou a terceira dose. Além disso, o gráfico apresenta a quarta dose, que tem sua aplicação em início com cerca de 1000 doses aplicadas.',
+    analise: 'A partir da visualização e análise do gráfico gerado é possível notar um decaimento na cobertura vacinal conforme as sucessivas doses foram surgindo. Tal dado pode ser considerado preocupante, visto que o normal seria a linha se manter "reta" e estável o que significaria uma alta adesão popular às aplicações das doses, algo que não aconteceu na prática.',
     grafico: 'https://i.imgur.com/u51B4ii.png'
 },{
     id:'1',
